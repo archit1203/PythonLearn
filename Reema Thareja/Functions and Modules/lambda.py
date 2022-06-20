65225e3034557b36baed9468fbc53f5aec506570
archit1203 <architbiz1203@gmail.com>
2022-06-20 20:35:29 +0530

    UPdate

---
+++ Reema Thareja/Functions and Modules/lambda.py
@@ -0,0 +1,12 @@
+"""
+Lambda Function
+->Throwaway functions
+->syntax:   lamda arguments : expression
+-> ',' seprates the arguments
+-> 
+"""
+
+"""Eg 1"""
+sum = lambda x , y : x+y
+
+print("Sum: ",sum(2,3))
