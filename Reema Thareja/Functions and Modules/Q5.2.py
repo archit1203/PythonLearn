a742aa7c4907a4895bf113be8d807d1b5732d938
archit1203 <architbiz1203@gmail.com>
2022-06-20 09:52:52 +0530

    Update 1

---
+++ Reema Thareja/Functions and Modules/Q5.2.py
@@ -0,0 +1,15 @@
+"""variable length argument"""
+# def fun(name,*fav_sub):
+#     print("\n",name,"likes to read")
+#     for subs in fav_sub:
+#         print(subs)
+
+# while True:
+#     print("Enter: \n")
+#     name=input("Name: ")
+#     subs=input("Subs: ")
+#     fun(name,subs)
+#     x=int(input("Continue: "))
+#     if x==-1:
+#         break
+
