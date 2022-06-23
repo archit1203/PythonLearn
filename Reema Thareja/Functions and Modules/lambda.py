dfce2c6ac4368c15be8cfa7532896945ba6231af
archit1203 <architbiz1203@gmail.com>
2022-06-23 11:29:04 +0530

    u

--- Reema Thareja/Functions and Modules/lambda.py
+++ Reema Thareja/Functions and Modules/lambda.py
@@ -11,12 +11,24 @@ Lambda Function
 
 # print("Sum: ",sum(2,3))
 
-def sumdif(x,y):
-    sm=a+b
-    dif=a-b
-    return sm*dif
+# def sumdif(x,y):
+#     sm=a+b
+#     dif=a-b
+#     return sm*dif
 
-a = int(input())
-b = int(input())
+# a = int(input())
+# b = int(input())
 
-print(sumdif(a,b))
+# print(sumdif(a,b))
+
+
+
+def solve(n):
+    for i in range(1,n+1):
+        for k in range(1,i+1):
+            print(k,end="")
+        for l in range(i-1,0,-1):
+            print(l,end="")
+        print("\n")
+    
+solve(5)
